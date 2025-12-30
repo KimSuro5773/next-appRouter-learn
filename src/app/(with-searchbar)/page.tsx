6d2d0e12e28e7b030386dd06303883e25a80c02f
Kimsuro <ss01056489656@gmail.com>
2025-12-30 14:54:06 +0900

    3.5) 리액트 서버 컴포넌트 주의사항
    - 1. 서버 컴포넌트에는 브라우저에서 실행될 코드가 포함되면 안된다.
    - 2. 클라이언트 컴포넌트는 클라이언트에서만 실행되지 않는다.
    - 3. 클라이언트 컴포넌트에서 서버 컴포넌트를 import 할 수 없다 (import시 서버 컴포넌트도 클라이언트 컴포넌트가 됨)
      - 만약에 클라이언트 컴포넌트가 서버 컴포넌트를 자식으로 두어야 한다면 children props로 받아서 렌더링하는게 좋음
    - 4. 서버 컴포넌트에서  클라이언트 컴포넌트에게 직렬화 되지 않는 Porps는 전달이 불가능하다. ex) 함수
      - RSC Payload 과정에서 함수를 클라이언트 컴포넌트에게 props로 전달하면 함수는 직렬화가 불가능 하기 때문에 런타임 에러 발생

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,14 @@
+import ClientComponent from "./client-component";
 import styles from "./page.module.css";
+import ServerComponent from "./server-components";
 
 export default function Home() {
-  return <div className={styles.page}>Index 페이지</div>;
+  return (
+    <div className={styles.page}>
+      Index 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  );
 }
