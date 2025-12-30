e21a97fc9d3fb0321f5afb5a3ec886331bd888d4
Kimsuro <ss01056489656@gmail.com>
2025-12-30 13:53:31 +0900

    3.4) 레이아웃 설정하기

--- src/app/search/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Page({ searchParams }: { searchParams: Promise<{ q: string }> }) {
-  const { q } = await searchParams;
-
-  console.log(q);
-
-  return <div>Search 페이지 : {q}</div>;
-}
